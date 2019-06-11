4879859ec9f1577e592ae6534e2593e6149d1bad
Roman Hargrave <roman@hargrave.info>
2019-06-11 12:47:49 -0500

    add port and proto

--- start.sh
+++ start.sh
@@ -4,7 +4,7 @@ conf_file=/etc/nginx/nginx.conf
 
 echo "Setting up nginx configuration"
 
-envsubst '${TARGET_HOST} ${TARGET_ADDRESS}' < "$conf_file.tpl" > "$conf_file"
+envsubst '${TARGET_HOST} ${TARGET_ADDRESS} ${TARGET_X_FORWARDED_PORT} ${TARGET_X_FORWARDED_PROTO}' < "$conf_file.tpl" > "$conf_file"
 
 cat "$conf_file"
 
